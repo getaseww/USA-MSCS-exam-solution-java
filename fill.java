377df61e8108795a28a5f32292a85bbd57c11a9d
Getasew <getas.walelign@gmail.com>
2023-05-12 21:23:10 +0300

    fill

--- fill.java
+++ fill.java
@@ -4,23 +4,25 @@
 class Main {
 
     public static int[] fill(int[] nums,int k,int n){
-        int[] newNums=new int[n];
+        int[] arr2 = new int[n];        
+        System.out.println("from fill class");
+        System.out.println(arr2);
         if(k<0||n<0){
-            return newNums;
+            return arr2;
         }
         int temp=0;
             for(int i=0;i<n;i++){
                 if(temp<k){
-                    newNums[i]=nums[temp];
+                    arr2[i]=nums[temp];
                     temp++;
                 }else if(temp==k){
                     temp=0;
-                    newNums[i]=nums[temp];
+                    arr2[i]=nums[temp];
                     temp++;
                 }
             }
-        System.out.print(newNums);    
-        return newNums;
+            System.out.println(Arrays);
+        return arr2;
     }
     public static void main(String[] args) {
         int[] nums={1,2,3,4,5,6,7};
--- hollow.java
+++ hollow.java
@@ -3,8 +3,10 @@ class Main {
 
     public static int isHollow(int[] nums) {
         if(nums.length<3) return 0;
-        for(int i=1;i<nums.length;i++){
-           
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0&&nums.length>3){
+                
+            }
         }
 
         return false;
